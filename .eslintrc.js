2bd4a147f3d17eb9be38ffeb2e1f34289ff3a13f
caiyufu <caiyufu@meituan.com>
2022-05-30 20:33:59 +0800

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'no-console': 2,
+    'quotes': ['error', 'single'],
+    'curly': 0,
+    'brace-style': ['error', 'stroustrup'],
+    'indent': ['error', 2],
+    'eol-last': ['error', 'never'],
+    '@typescript-eslint/no-unused-vars': 2
+  }
+};
--- demo/demo-action/src/game/Game.tsx
+++ demo/demo-action/src/game/Game.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useRef } from 'react'
-import Phaser from 'phaser'
+import React, { useEffect, useRef } from 'react';
+import Phaser from 'phaser';
 
-import logoImg from '../assets/logo.png'
+import logoImg from '../assets/logo.png';
 
 export const Game = () => {
   useEffect(() => {
-    var config = {
+    const config = {
       type: Phaser.AUTO,
       width: 800,
       height: 600,
       scene: {
         preload: function () {
-          this.load.image("logo", logoImg);
+          this.load.image('logo', logoImg);
         },
         create: function () {
-          const logo = this.add.image(400, 150, "logo");
+          const logo = this.add.image(400, 150, 'logo');
         },
         update: () => {}
       }
     };
-    var game = new Phaser.Game(config);
-  }, [])
+    const game = new Phaser.Game(config);
+  }, []);
   return (
     <div>
     </div>
-  )
-}
+  );
+};
